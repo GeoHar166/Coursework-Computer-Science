44a65399099f81c5452e47da8bdf5017ec1cff1a
GeoHar166 <har166@theleys.net>
2025-10-14 12:35:55 +0100

    dykstras algorithm complete next need to implement it into car.move so far the algorithm finds the best node path from node1 to node2 and prints this

--- nodetrial.py
+++ nodetrial.py
@@ -15,7 +15,7 @@ class car:
 
         self.start = start
         self.finish = finish
-        self.x,self.y = lanelinks[start][1]
+        self.startx,self.starty = lanelinks[start][1]
         self.finishx,self.finishy = lanelinks[finish][1]
         
         self.target = 0
@@ -24,15 +24,15 @@ class car:
 
     def draw(self): 
         rotated_img = pygame.transform.rotate(self.car_img, self.angle) 
-        rect = rotated_img.get_rect(center=(self.x, self.y)) 
+        rect = rotated_img.get_rect(center=(self.startx, self.starty)) 
         screen.blit(rotated_img, rect)
 
 #    def createpath(self,endx,endy):
     
     def move(self):  #alternate move() function
         try:
-            dx = self.finishx - self.x
-            dy = self.finishy - self.y
+            dx = self.finishx - self.startx
+            dy = self.finishy - self.starty
         except IndexError:
             dx,dy = 0,0
 
@@ -43,7 +43,7 @@ class car:
 
         # if distance < self.speed:
         #     try:
-        #         self.x, self.y = self.path[self.target]   
+        #         self.startx, self.starty = self.path[self.target]   
         #         self.target += 1    # so that the car does not drive past the checkpoint    
         #     except IndexError:  #if car reaches end before others, do nothing
         #         pass
@@ -57,19 +57,13 @@ class car:
         else:
             dir_x,dir_y = 0,0
         if self.speed > 0:
-            self.x += dir_x * self.speed
-            self.y += dir_y * self.speed
+            self.startx += dir_x * self.speed
+            self.starty += dir_y * self.speed
 
             angle_rad = math.atan2(-dir_y, dir_x)       # 0 is right, 180 left, 90 up, -90 down
             self.angle = math.degrees(angle_rad)        #updated angle algorithm    
 
-    def find_node_distance(self,node1,node2):
-        distanceAx = lanelinks[node1][1][0]-lanelinks[node2][1][0]  # x-x
-        distanceBy = lanelinks[node1][1][1]-lanelinks[node2][1][1]  # y-y
-        distanceCtotal = math.sqrt((distanceAx**2)+(distanceBy**2)) # a^2 + b^2 = c^2
-        return distanceCtotal
 
-      
 
     def nearcar(self,cars):
         closecars = 0
@@ -84,7 +78,7 @@ class car:
                     if car is self: #dont compare with itself
                         continue
 
-                    distance = math.hypot(self.x - car.x, self.y - car.y )  # distance between car and other cars on the road
+                    distance = math.hypot(self.startx - car.x, self.starty - car.y )  # distance between car and other cars on the road
 
                     if distance <= 110:      #if car is close and going the same direction, stop!
                         if int(-1*self.angle) + 180 == int(-1*car.angle) or int(-1*self.angle) - 180 == int(-1*car.angle):  
@@ -95,7 +89,7 @@ class car:
                                 self.behindcar = True
                                 if self.speed >= car.speed:
                                     self.speed = car.speed
-                                if abs(self.x - car.x) < 100 or abs(self.y - car.y) < 100:
+                                if abs(self.startx - car.x) < 100 or abs(self.starty - car.y) < 100:
                                     self.speed -= self.speed/10
                                 closecars += 1
                             
@@ -104,7 +98,7 @@ class car:
                                 self.behindcar = True
                                 self.speed = car.speed
 
-                            if abs(self.x - car.x) < 100 or abs(self.y - car.y) < 100:
+                            if abs(self.startx - car.x) < 100 or abs(self.starty - car.y) < 100:
                                 self.speed -= self.speed/10
 
                             else:
@@ -131,11 +125,81 @@ def lanedraw(screen,start,end):
         #pygame.draw.line(screen,colour,(x,y),(x,y),width)
         pygame.draw.line(screen,(180,180,180),start,end,50)     #change so width is 100 when 2 way road
 
+
+#GLOBAL DYKSTRA FUNCTIONS:
+def dykstras_nodes_create():
+    nodes = {}
+    for link in lanelinks.keys():
+        nodes[link] = {}            
+        for linklink in lanelinks[link][0]:
+            dist = find_node_distance(link,linklink)
+            nodes[link][linklink] = dist #+ self.traffic_weight
+    return nodes
+
+def find_node_distance(node1,node2):
+    distanceAx = lanelinks[node1][1][0]-lanelinks[node2][1][0]  # x-x
+    distanceBy = lanelinks[node1][1][1]-lanelinks[node2][1][1]  # y-y
+    distanceCtotal = math.sqrt((distanceAx**2)+(distanceBy**2)) # a^2 + b^2 = c^2
+    return distanceCtotal
+
+
+def dykstras_table(visited,unvisited,table,nodes):   #UNFINISHED
+    smallestnode = unvisited[0]
+
+    for node in table:  #choose the smallest (distance) unvisited node
+        if node in unvisited:
+            if table[node][1] < table[smallestnode][1]:
+                smallestnode = node
+
+    currentnode = smallestnode
+
+    for node in nodes[currentnode]:
+        shortestpathposibility = table[currentnode][1] + int(nodes[currentnode][node])
+
+        if table[node][1] >= shortestpathposibility:
+            table[node][1] = shortestpathposibility #   shortestdist = dist
+            table[node][0] = currentnode            #   previousnode = currentnode
+
+    visited.append(currentnode)
+    unvisited.remove(currentnode)
+    return visited,unvisited,table
+
+def dykstrapath(firstnode,endnode,table):  #UNFINISHED
+    #SELECT PATH
+    distance = table[endnode][1]
+
+    path = [endnode]
+    nodefind = endnode
+    while path[0] != firstnode:
+        path.insert(0,table[nodefind][0])
+        nodefind = table[nodefind][0]
+    return path
+
+
+def dykstras(firstnode,nodes):   #UNFINISHED
+
+    visited = []
+    unvisited = list(lanelinks.keys())
+    table = {}
+
+    for node in lanelinks:
+        #node   prevnode    shortestdist
+        table[node] = [None,9999]
+    table[firstnode] = [None,0]
+
+
+
+    while len(unvisited) != 0:
+        visited,unvisited,table = dykstras_table(visited,unvisited,table,nodes)
+    
+    return table
+
+
 #lanelinks[A][0] = links
 #lanelinks[A][1] = properties (x,y)
 
 lanelinks = {
-    "A" : [["J","B","D"],[25,575]], #  +25 to coordinate to have lane locked into 
+    "A" : [["J","B","D"],[25,575]], #  +25 to coordinate to have lane locked into grid 
     "B" : [["A","C"],[25,25]],
     "C" : [["B","D"],[275,25]],
     "D" : [["C","E","A"],[275,575]],
@@ -147,44 +211,41 @@ lanelinks = {
     "J" : [["I","A"],[275,875]]
 
 }
+    #GLOBAL VARS
+# colours for later use
+white = (255, 255, 255)
+red = (180, 0, 0)
+green = (30,180,30)
+blue = (0, 0, 180)
+black = (0,0,0)
+grey = (180,180,180)
+
+width = 1800        
+height = 1000       #pixel size of window
+
+screen = pygame.display.set_mode((width,height))    #make screen for display
+
 
 def main():    
 
-    # GLOBAL VARs
-    global screen,white,black,grey
-
-    width = 1800        
-    height = 1000       #pixel size of window
-    screen = pygame.display.set_mode((width,height))    #make screen for display
-
-
     running = True
-
-    # colours for later use
-    white = (255, 255, 255)
-    red = (180, 0, 0)
-    green = (30,180,30)
-    blue = (0, 0, 180)
-    black = (0,0,0)
-    grey = (180,180,180)
-
-    car1 = car(1,"B","I")
-    
-
+    car1 = car(1,"B","H")
 
 #DRAW THIS BEFORE LOOP STARTS AS IT DOESNT CHANGE
-
-
 
         # rect = pygame.Rect(0,0,100,100)
         # rect.center = (lanelinks[node][1])
         # pygame.draw.rect(screen,(180,180,180),rect)
 
-
-    
-
     clock = pygame.time.Clock()
     fps = 300
+
+
+    nodes = dykstras_nodes_create()
+    table = dykstras(car1.start,nodes)
+    path = dykstrapath(car1.start,car1.finish,table)
+    print(path)
+
 
     while running:
         
@@ -197,7 +258,10 @@ def main():
         clock.tick(fps)
 
 #DRAW HERE FOR CHANGING THINGS
-        
+
+        # for car in cars:
+        #     # if coords == coords_of_node:
+        #     #     redo dykstras
 
 
         screen.fill(green) #clear screen so no repeated "draws"
